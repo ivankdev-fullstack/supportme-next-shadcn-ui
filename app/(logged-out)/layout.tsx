ef42e5bdbb2a76ff93bb88f2fe5f003749606be7
ivankdev-fullstack <ivan.kononenko.dev@gmail.com>
2024-12-18 15:35:32 +0200

    feat: styling the homepage

---
+++ app/(logged-out)/layout.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+type Props = {
+  children?: React.ReactNode;
+};
+
+const LoggedOutLayout = ({ children }: Props) => {
+  return (
+    <div className="flex flex-col gap-4 min-h-screen items-center justify-center p-24">
+      {children}
+    </div>
+  );
+};
+
+export default LoggedOutLayout;
--- app/(logged-out)/page.tsx
+++ app/(logged-out)/page.tsx
@@ -1,12 +1,25 @@
 import { Button } from "@/components/ui/button";
+import { PersonStandingIcon } from "lucide-react";
+import Link from "next/link";
 
 const LandingPage = () => {
   return (
-    <div>
-      <h1>LandingPage</h1>
-      <Button>Log in</Button>
-      <Button>Sign up</Button>
-    </div>
+    <>
+      <h1 className="flex gap-2 items-center">
+        <PersonStandingIcon size={50} className="text-pink-500" />
+        SupportMe
+      </h1>
+      <p>The best dashboard to manage customer support</p>
+      <div className="flex gap-2 items-center">
+        <Link href="login">
+          <Button>Log in</Button>
+        </Link>
+        <small>or</small>
+        <Link href="signup">
+          <Button variant="outline">Sign up</Button>
+        </Link>
+      </div>
+    </>
   );
 };
 
