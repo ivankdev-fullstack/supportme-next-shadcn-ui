b7202841025b9f51df81d39fa42157bf29060f09
ivankdev-fullstack <ivan.kononenko.dev@gmail.com>
2024-12-18 15:20:25 +0200

    feat: install shadui, add text and buttons to the homepage

---
+++ app/(logged-out)/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+
+const LandingPage = () => {
+  return (
+    <div>
+      <h1>LandingPage</h1>
+      <Button>Log in</Button>
+      <Button>Sign up</Button>
+    </div>
+  );
+};
+
+export default LandingPage;
